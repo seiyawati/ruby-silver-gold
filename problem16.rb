1420cd30abeafe472a21b0449e7cf726ff777e5d
Seiya Kawamoto <kid960805@gmail.com>
2021-11-24 23:13:29 +0900

    add: problem16

---
+++ problem16.rb
@@ -0,0 +1,5 @@
+mozart = ["Symphony","Piano Concerto", "Violin Concerto","Horn Concerto","Violin Sonata"]
+listend = ["Symphony","Violin Concerto","Horn Concerto"]
+p mozart__(1)__listend
+[出力]
+["Piano Conerto","Violin Sonata"]

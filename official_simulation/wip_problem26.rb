7b3974187ba79118a1de24360e5a34630f59dc76
Seiya Kawamoto <kid960805@gmail.com>
2021-11-27 18:32:05 +0900

    wip: problem26

--- official_simulation/wip_problem26.rb
+++ official_simulation/wip_problem26.rb
@@ -1,6 +1,7 @@
 s = "To be or not to be, that is the question."
 hash = Hash.new(0)
 
+p s.scan(/\w+/) # -> ["To", "be", "or", "not", "to", "be", "that", "is", "the", "question"]
 s.scan(/\w+/) {|i| hash[i] += 1}
 # s.scan(/\w+/) do |i| 上記と同じ意味
 #   hash[i] += 1

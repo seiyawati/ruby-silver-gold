9a7e0ca64998c04beb88ead4fa5f598fb4c2c537
Seiya Kawamoto <kid960805@gmail.com>
2021-11-27 15:36:49 +0900

    add: problem44

---
+++ official_simulation/problem44.rb
@@ -0,0 +1,7 @@
+a ={:Sweden =>"Stockholm", :Norway=>"Oslo", :Finland=>"Helsinki"}
+b = {Sweden: "Stockholm", Norway: "Oslo", Finland: "Helsinki"}
+c = Hash[:Sweden,  "Stockholm", :Norway ,"Oslo", :Finland, "Helsinki"]
+
+p a
+p b
+p c

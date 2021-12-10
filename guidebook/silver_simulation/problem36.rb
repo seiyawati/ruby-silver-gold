a3df8c5cd65229e82ec79f15a84a82a5a95452f0
Seiya Kawamoto <kid960805@gmail.com>
2021-12-10 17:13:38 +0900

    fix: problem36

--- guidebook/silver_simulation/problem36.rb
+++ guidebook/silver_simulation/problem36.rb
@@ -1,3 +1,8 @@
 # パターンに最初にマッチした文字列を返す。
 p "hogepiyohogehoge".slice(/o../)
 # -> oge
+
+str = "milkman !!"
+puts str.slice(2)  # -> l
+puts str.slice(1..4) # -> ilkm
+puts str.slice(/\w+/) # -> milkman

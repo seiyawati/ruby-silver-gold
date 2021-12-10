b7420e79423b59f0a08ca4ebfe002c64147b44af
Seiya Kawamoto <kid960805@gmail.com>
2021-12-10 23:55:34 +0900

    fix: problem2

--- guidebook/silver_practice_problem/problem2.rb
+++ guidebook/silver_practice_problem/problem2.rb
@@ -7,3 +7,15 @@ def hoge
   puts x
 end
 hoge
+
+# ---------------------------------------------------
+
+def hoge
+  x = 0
+  (1...5).each do |i|
+    # メソッド外のxを参照できない。
+    x += 1
+  end
+  puts x
+end
+hoge
